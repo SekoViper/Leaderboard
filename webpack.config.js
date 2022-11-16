6b6b12ad4689fe11a15c3ea2e98a3ccc3059f1ab
SekoVper <damehlivingstone1@gmail.com>
2022-11-16 16:23:25 +0100

    Modify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,70 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   entry: './src/index.js',
+//   mode: 'development',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     clean: true,
+//   },
+//   optimization: {
+//     runtimeChunk: 'single',
+//   },
+//   devServer: {
+//     static: './dist',
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       title: 'Output Management',
+//       template: './src/index.html',
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+// };
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
   mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+  entry: {
+    index: './src/index.js',
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
       template: './src/index.html',
     }),
   ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
